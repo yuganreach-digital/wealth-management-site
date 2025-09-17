49ac01ba436ff27d77fffebfed9b1b1f54b7742d
Vignesh <58678924+vignesh-shiftX@users.noreply.github.com>
2025-09-17 12:05:39 +0530

    send mail setup

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly GMAIL_USER: string;
+  readonly GMAIL_APP_PASSWORD: string;
+  readonly CONTACT_EMAIL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
